bef631dd8d48711cb5ae3dede6bc197e16e044a1
fahnaphat <125748126+fahnaphat@users.noreply.github.com>
2023-06-10 12:33:46 +0700

    Delete test.js

--- test.js
+++
@@ -1,5 +0,0 @@
-//Use require for use function in myModules.js
-const myModules = require('./modules/myModules')             //input path module file. 
-
-console.log(myModules.getCurrentTime())
-console.log(myModules.add(50,100))
